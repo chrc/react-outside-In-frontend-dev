363e4ed59a104b5ff7ec795ed4fd8973304d5086
Christophe Charles <christophe.charles@octo.com>
2021-01-12 15:58:33 +0100

    Add New Restaurant Form component

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
 
 import RestaurantList from './RestaurantList';
+import NewRestaurantForm from './NewRestaurantForm';
 
 const RestaurantScreen = () => (
   <Card>
     <CardContent>
       <Typography variant="h5">Restaurants</Typography>
+      <NewRestaurantForm />
       <RestaurantList />
     </CardContent>
   </Card>
